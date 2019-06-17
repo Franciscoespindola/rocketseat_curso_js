31da417ce71fe0f13a862ce600f524ba7cdf1dd9
unknown <francisco_espindola@saojose.sc.senai.br>
2019-06-17 08:45:14 -0300

    aula 2 mod 3

---
+++ mod1/aulas/main.js/mais.js
@@ -0,0 +1,8 @@
+var lista = document.querySelector("#app ul")
+var caixa = document.querySelector("#app input")
+var botao = document.querySelector("#app button")
+
+
+console.log(lista)
+    console.log(caixa)
+        console.log(botao)
